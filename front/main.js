b4d820b2c45b91079757341b64511955f3e2df6d
Remi Bartel <remi.bartel@mobilenowgroup.com>
2019-03-07 18:29:14 +0800

    udpdating

--- front/main.js
+++ front/main.js
@@ -1,26 +1,10 @@
-// const fetchUsers = async() => {
-//   try {
-//     await (await fetch('/.netlify/functions/getusers')).json();
-//   }
-//   catch(e) {
-//     console.log('ERROR', e)
-//   }
-// };
+modules.exports = test;
 
-console.log('test');
-// fetchUsers()
-//   .then(data => {
-//     let userList = document.querySelector('#users');
-//
-//     data.forEach(user => {
-//       const li = document.createElement('li');
-//       const link = document.createElement('a');
-//
-//       li.class = 'list-group-item';
-//       link.appendChild(document.createTextNode(user.login));
-//       link.href = user.html_url;
-//       link.target = '_blank';
-//       li.appendChild(link);
-//       userList.appendChild(li);
-//     })
-//   });
+const test = async() => {
+  try {
+    await (await fetch('/.netlify/dist/test')).json();
+  }
+  catch(e) {
+    console.log('ERROR', e)
+  }
+};
