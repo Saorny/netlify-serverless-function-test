fff871535045d5d0ab3952a99886ce16ccdebffb
Remi Bartel <remi.bartel@mobilenowgroup.com>
2019-03-07 16:47:27 +0800

    udpdating

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
-const fetchUsers = async() => {
-  try {
-    await (await fetch('/.netlify/functions/getusers')).json();
-  }
-  catch(e) {
-    console.log('ERROR', e)
-  }
-};
+// const fetchUsers = async() => {
+//   try {
+//     await (await fetch('/.netlify/functions/getusers')).json();
+//   }
+//   catch(e) {
+//     console.log('ERROR', e)
+//   }
+// };
 
-fetchUsers()
-  .then(data => {
-    let userList = document.querySelector('#users');
-
-    data.forEach(user => {
-      const li = document.createElement('li');
-      const link = document.createElement('a');
-
-      li.class = 'list-group-item';
-      link.appendChild(document.createTextNode(user.login));
-      link.href = user.html_url;
-      link.target = '_blank';
-      li.appendChild(link);
-      userList.appendChild(li);
-    })
-  });
+console.log('fetch users');
+// fetchUsers()
+//   .then(data => {
+//     let userList = document.querySelector('#users');
+//
+//     data.forEach(user => {
+//       const li = document.createElement('li');
+//       const link = document.createElement('a');
+//
+//       li.class = 'list-group-item';
+//       link.appendChild(document.createTextNode(user.login));
+//       link.href = user.html_url;
+//       link.target = '_blank';
+//       li.appendChild(link);
+//       userList.appendChild(li);
+//     })
+//   });
